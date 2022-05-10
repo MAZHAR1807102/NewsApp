d224fb7c4d6159c2ed5d9184107c92d397c0ddb3
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-10 13:32:02 +0600

    Update second time

---
+++ app/src/new/javafile.java
@@ -0,0 +1,26 @@
+package new
+public class javafile {
+
+    int var1;
+    String Str;
+    javafile()
+    {
+
+    }
+
+    public int getVar1() {
+        return var1;
+    }
+
+    public String getStr() {
+        return Str;
+    }
+
+    public void setVar1(int var1) {
+        this.var1 = var1;
+    }
+
+    public void setStr(String str) {
+        Str = str;
+    }
+}
