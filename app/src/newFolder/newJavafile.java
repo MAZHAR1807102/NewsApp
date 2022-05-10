e82fff2c3a1acf8fd27a3e460020b39595152340
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-10 13:41:58 +0600

    new main update

---
+++ app/src/newFolder/newJavafile.java
@@ -0,0 +1,27 @@
+package newFolder
+
+public class newJavafile
+        {
+            int a1;
+            float f1;
+            public newJavafile(int a1, float f1) {
+                this.a1 = a1;
+                this.f1 = f1;
+            }
+
+            public int getA1() {
+                return a1;
+            }
+
+            public void setA1(int a1) {
+                this.a1 = a1;
+            }
+
+            public float getF1() {
+                return f1;
+            }
+
+            public void setF1(float f1) {
+                this.f1 = f1;
+            }
+        }
